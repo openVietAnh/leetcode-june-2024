475a649feec35ff355ece3c062e5f67875811b67
anhtv <anh.tv@teko.vn>
2024-06-24 12:39:54 +0700

    clean code

--- src/solutions/min_k_bit_flips.rs
+++ src/solutions/min_k_bit_flips.rs
@@ -1,16 +1,16 @@
 pub fn min_k_bit_flips(nums: Vec<i32>, k: i32) -> i32 {
     let mut result = 0;
-    let mut isFlipped = vec![0; nums.len()];
+    let mut is_flipped = vec![0; nums.len()];
     let mut flipped = 0;
     for i in 0..nums.len() {
-        if (i >= k as usize) {
-            flipped ^= isFlipped[i - k as usize];
+        if i >= k as usize {
+            flipped ^= is_flipped[i - k as usize];
         }
-        if (flipped == nums[i]) {
-            if (i + k as usize > nums.len()) {
+        if flipped == nums[i] {
+            if i + k as usize > nums.len() {
                 return -1;
             }
-            isFlipped[i] = 1;
+            is_flipped[i] = 1;
             flipped ^= 1;
             result += 1;
         }
